202a49772a90cf62edf4df0390e6df8959203e59
JanaHsen <janahsen5@gmail.com>
2025-02-20 01:20:31 +0200

    The inputs we need for the program

---
+++ JanaHussein-Assignment1.py
@@ -0,0 +1,14 @@
+#All the inputs we need 
+# the user must enter the salary first
+Nabihas_salary = int(input("Please enter your salary  "))
+
+#the user must specify the month of the salary
+Salary_month = input('Please enter the corresponding month ')
+
+#the user must input the percentage of savings, rent and electricity
+savings_percent = int(input("Please enter the percentage for savings  "))
+
+rent_percent = int(input("Please enter the percentage for rent "))
+
+elect_percent = int(input("Please enter the percentage for electricity "))
+
